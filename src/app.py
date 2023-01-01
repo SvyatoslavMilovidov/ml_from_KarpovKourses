66ad5b0aed8fe67917c7439be26938b1f149d433
SvyatoslavMilovidov <ponik_mira2@mail.ru>
2023-01-01 19:35:53 +0800

    add /sum_date

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,9 @@ app = FastAPI()
 @app.get('/')
 def get_hello_world(a: int, b: int):
     return a + b
+
+
+@app.get('/sum_date')
+def get_sum_date(current_date: datetime.date, offset: int):
+    diff_days = datetime.timedelta(days=offset)
+    return current_date + diff_days
