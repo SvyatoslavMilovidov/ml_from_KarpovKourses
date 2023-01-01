fbbcda57735971ef30666276f00a06c7fb97ceb5
SvyatoslavMilovidov <ponik_mira2@mail.ru>
2023-01-01 22:02:49 +0800

    add /post/{post_id}

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import datetime
 
 import psycopg2
-from psycopg2.extras import RealDictCursor
-from loguru import logger
 from fastapi import FastAPI, HTTPException, Depends
+from loguru import logger
+from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -14,6 +14,15 @@ class User(BaseModel):
     surname: str
     age: int
     registration_date: datetime.date
+
+    class Config:
+        orm_mode = True
+
+
+class PostResponse(BaseModel):
+    id: int
+    text: str
+    topic: str
 
     class Config:
         orm_mode = True
@@ -46,12 +55,12 @@ def get_sum_date(current_date: datetime.date, offset: int):
 def get_user_info(user_id: int, db=Depends(get_db)):
     result = None
     with db.cursor() as cursor:
-        sql_text = f'''
+        sql_request = f'''
         SELECT gender, age, city 
             FROM "user"
         WHERE id={user_id}
         '''
-        cursor.execute(sql_text)
+        cursor.execute(sql_request)
         result = cursor.fetchone()
     logger.info(f'Get info: {result}')
     if result:
@@ -62,3 +71,19 @@ def get_user_info(user_id: int, db=Depends(get_db)):
 @app.post('/user/validate')
 def post_user_validate(user_info: User):
     return f'Will add user: {user_info.name} {user_info.surname} with age {user_info.age}'
+
+
+@app.post('/post/{post_id}', response_model=PostResponse)
+def post_post_info(post_id: int, db=Depends(get_db)) -> PostResponse:
+    result = None
+    with db.cursor() as cursor:
+        sql_request = f'''
+        SELECT id, text, topic 
+            FROM "post"
+        WHERE id={post_id}
+        '''
+        cursor.execute(sql_request)
+        result = cursor.fetchone()
+    # if result:
+    return PostResponse(**result)
+    # raise HTTPException(404, 'post not found')
